94ddb0628581c1dba8c878450988a26020a72072
Abdelrahman Soltan <asoltan@instabug.com>
2023-02-03 20:06:48 +0200

    api: 👽️ add products APIs

--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,13 @@
+const allProductsUrl = 'https://course-api.com/javascript-store-products';
+
+// temporary single product
+// 'https://course-api.com/javascript-store-single-product?id=rec43w3ipXvP28vog'
+const singleProductUrl = 'https://course-api.com/javascript-store-single-product';
+
 const getElement = selection => {
   const element = document.querySelector(selection);
   if (element) return element;
   throw new Error(`Please check "${selection}" selector, no such element exist`);
 };
 
-export { getElement };
-
+export { getElement, allProductsUrl, singleProductUrl };
