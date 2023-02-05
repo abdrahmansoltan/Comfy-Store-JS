4faba068f981720cf56ce91e6759672923e061d3
Abdelrahman Soltan <asoltan@instabug.com>
2023-02-05 06:59:28 +0200

    feat(Cart): :sparkles: create utility funtions for handling cart-items functionalities

--- src/cart/setupCart.js
+++ src/cart/setupCart.js
@@ -23,16 +23,72 @@ export const addToCart = id => {
   } else {
     // 2. If item already exists in the cart
     // Update values
-    const amount = 1; // TEMP
+    const amount = increaseAmount(id);
     const items = [...cartItemsDOM.querySelectorAll('.cart-item-amount')];
     const newAmount = items.find(value => value.dataset.id === id);
     newAmount.textContent = amount;
   }
   // add one to the item count
-
+  displayCartItemCount();
   // display cart totals
-  
+  displayCartTotal();
   // set cart in local storage
   setStorageItem('cart', cart);
   openCart();
 };
+
+// ---------------- utils ---------------- //
+function displayCartItemCount() {
+  const amount = cart.reduce((total, cartItem) => {
+    return (total += cartItem.amount); // amount is how many items
+  }, 0);
+  cartItemCountDOM.textContent = amount;
+}
+function displayCartTotal() {
+  let total = cart.reduce((total, cartItem) => {
+    return (total += cartItem.price * cartItem.amount);
+  }, 0);
+  cartTotalDOM.textContent = `Total : ${formatPrice(total)} `;
+}
+function displayCartItemsDOM() {
+  cart.forEach(cartItem => {
+    addToCartDOM(cartItem);
+  });
+}
+function removeItem(id) {
+  cart = cart.filter(cartItem => cartItem.id !== id);
+}
+function increaseAmount(id) {
+  let newAmount;
+  cart = cart.map(cartItem => {
+    if (cartItem.id === id) {
+      newAmount = cartItem.amount + 1;
+      cartItem = { ...cartItem, amount: newAmount };
+    }
+    return cartItem;
+  });
+  return newAmount;
+}
+function decreaseAmount(id) {
+  let newAmount;
+  cart = cart.map(cartItem => {
+    if (cartItem.id === id) {
+      newAmount = cartItem.amount - 1;
+      cartItem = { ...cartItem, amount: newAmount };
+    }
+    return cartItem;
+  });
+  return newAmount;
+}
+
+// starter function to call all functions on every page load
+const init = () => {
+  // display amount of cart items
+  displayCartItemCount();
+  // display total
+  displayCartTotal();
+  // add all cart items to the dom
+  displayCartItemsDOM();
+  // setup cart functionality
+};
+init();
